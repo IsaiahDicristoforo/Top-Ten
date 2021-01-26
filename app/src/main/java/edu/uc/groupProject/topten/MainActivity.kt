5818bddbfe6cd763f0a82a979445cfaf50f22042
Isaiah Dicristoforo <48969369+IsaiahDicristoforo@users.noreply.github.com>
2021-01-26 12:38:49 -0500

    Initial commit

---
+++ app/src/main/java/edu/uc/groupProject/topten/MainActivity.kt
@@ -0,0 +1,18 @@
+package edu.uc.groupProject.topten
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.uc.groupProject.topten.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
