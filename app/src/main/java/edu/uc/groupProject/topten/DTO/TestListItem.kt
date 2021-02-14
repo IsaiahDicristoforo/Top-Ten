32f54e8ec56cd4573adbc1ecc46cec8f894014e1
Isaiah Dicristoforo <48969369+IsaiahDicristoforo@users.noreply.github.com>
2021-02-13 23:38:28 -0500

    Now a collection list items can be added to the lists in the Firestore database.

---
+++ app/src/main/java/edu/uc/groupProject/topten/DTO/TestListItem.kt
@@ -0,0 +1,5 @@
+package edu.uc.groupProject.topten.DTO
+
+data class TestListItem(var title:String, var description:String="", var totalVotes:Int){
+
+}
--- app/src/main/java/edu/uc/groupProject/topten/MainActivity.kt
+++ app/src/main/java/edu/uc/groupProject/topten/MainActivity.kt
@@ -3,9 +3,12 @@ package edu.uc.groupProject.topten
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
+import com.google.firebase.firestore.model.Document
 import edu.uc.groupProject.topten.DTO.TestDTO
+import edu.uc.groupProject.topten.DTO.TestListItem
 import edu.uc.groupProject.topten.ui.main.MainFragment
 import java.util.*
 
@@ -33,13 +36,37 @@ class MainActivity : AppCompatActivity() {
         firestore = FirebaseFirestore.getInstance()
         firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
 
-        var testList = TestDTO("Top Ten Movies", false, true, true, Date())
+        var testList = TestDTO("Top Fifteen Movies", false, true, true, Date())
 
-        firestore.collection("lists").document("Top Ten Movies").set(testList).addOnSuccessListener {
+
+        var  listsReference = (firestore.collection("lists").document(testList.listName))
+
+
+        listsReference.set(testList).addOnSuccessListener {
             Log.d("Firebase", "document saved");
         }.addOnFailureListener{
             Log.d("Firebase", "Save Failed");
         }
+
+        var listItemCollectionReference = listsReference.collection("MyListItems")
+
+        var arrayOfListItemsToAdd : Array<TestListItem> = createSampleListItems();
+
+        for(item in arrayOfListItemsToAdd){
+            listItemCollectionReference.document(item.title).set(item);
+        }
+
+    }
+
+    fun createSampleListItems():Array<TestListItem>{
+
+        var sampleListItems = arrayOf(
+                TestListItem("The Dark Knight", "A movie about Batman", 100),
+                TestListItem("The Return of the King", "A movie about a ring and some eagles", 150),
+                TestListItem("The Empire Strikes Back", "A movie about some light wands and parent issues", 200)
+
+        )
+        return sampleListItems
     }
 
 
