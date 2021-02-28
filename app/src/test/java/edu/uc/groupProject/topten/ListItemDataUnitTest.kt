2ae59ae6d869e13b45096fc061f4f2f193a76e69
Kyle Marler <marlerkw@mail.uc.edu>
2021-02-27 22:08:39 -0500

    Added a new function to the List Item unit test. Refactored class name of "ExampleUnitTest" to "ListItemDataUnitTest". Added additional variable validations to "thenResultContainsAvengers" function. Change the a string in "MainActivity.kt"

--- app/src/test/java/edu/uc/groupProject/topten/ListItemDataUnitTest.kt
+++ app/src/test/java/edu/uc/groupProject/topten/ListItemDataUnitTest.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import edu.uc.groupProject.topten.DTO.ListItem
 import edu.uc.groupProject.topten.Service.ListService
 import edu.uc.groupProject.topten.ui.main.MainViewModel
+import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
 import org.junit.Rule
@@ -18,7 +20,7 @@ import org.junit.rules.TestRule
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class ListItemDataUnitTest {
     @get:Rule
     var rule: TestRule =  InstantTaskExecutorRule()
     lateinit var mvm: MainViewModel
@@ -30,15 +32,28 @@ class ExampleUnitTest {
         givenAListOfMockItemsAreAvailable()
         whenSearchForMovies()
         thenResultContainsAvengers()
+        thenVerifyFunctionsInvoked()
     }
 
     private fun givenAListOfMockItemsAreAvailable(){
-        mvm = MainViewModel();
+        mvm = MainViewModel()
         createMockData()
+    }
+    private fun createMockData() {
+        var allListItemsLiveData = MutableLiveData<ArrayList<ListItem>>()
+        var allListItems = ArrayList<ListItem>()
+
+        var theDarkKnight = ListItem("The Dark Knight", "The best batman movie", 2)
+        var theAvengers = ListItem("The Avengers", "They all team up to fight bad guys", 4)
+        allListItems.add(theDarkKnight)
+        allListItems.add(theAvengers)
+
+        allListItemsLiveData.postValue(allListItems)
+        every { listService.fetchList(any<String>()) } returns allListItemsLiveData
+        mvm.listService = listService
     }
 
     private fun whenSearchForMovies(){
-
         mvm.fetchList("Top Ten Comic Book Movies")
     }
 
@@ -48,7 +63,7 @@ class ExampleUnitTest {
             assertNotNull(it)
             assertTrue(it.size > 0)
             it.forEach{
-                if(it.title.equals("The Avengers")){
+                if(it.title == "The Avengers" &&  it.description == "They all team up to fight bad guys" && it.totalVotes == 4){
                     avengersFound = true
                 }
             }
@@ -56,20 +71,11 @@ class ExampleUnitTest {
         assertTrue(avengersFound)
     }
 
-    private fun createMockData() {
-        var allListItemsLiveData = MutableLiveData<ArrayList<ListItem>>()
-        var allListItems = ArrayList<ListItem>()
 
-        var theDarkKnight = ListItem("The Dark Knight", "The best batman movie", 2)
-        var theAvengers = ListItem("The Avengers", "They all team up to fight bad guys", 4)
-        var inception = ListItem("Inception", "Yes.", 5)
-        allListItems.add(theDarkKnight)
-        allListItems.add(theAvengers)
-        allListItems.add(inception)
-
-        allListItemsLiveData.postValue(allListItems)
-        every { listService.fetchList(any<String>()) } returns allListItemsLiveData
-        mvm.listService = listService
+    private fun thenVerifyFunctionsInvoked() {
+        verify { listService.fetchList("Top Ten Comic Book Movies") }
+        verify (exactly = 0) { listService.fetchList("Top Ten Romance-Comedy Movies")}
+       confirmVerified(listService)
     }
 }
 
