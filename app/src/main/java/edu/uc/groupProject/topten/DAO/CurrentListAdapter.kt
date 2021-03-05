3229e60842de96dc5258f7aa0778d55eb88fe836
benjamin gomori <benjamingomori@gmail.com>
2021-03-05 03:48:19 -0500

    Removed database logic from CurrentListAdapter.kt. Added to voteButton handler logic to disable the button after vote, and to update UI count programmatically.

--- app/src/main/java/edu/uc/groupProject/topten/DAO/CurrentListAdapter.kt
+++ app/src/main/java/edu/uc/groupProject/topten/DAO/CurrentListAdapter.kt
@@ -1,20 +1,21 @@
 package edu.uc.groupProject.topten.DAO
 
-import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
-import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirebaseFirestoreSettings
 import edu.uc.groupProject.topten.DTO.ListItem
 import edu.uc.groupProject.topten.R
-import org.w3c.dom.Text
+import edu.uc.groupProject.topten.ui.main.MainViewModel
 
-class CurrentListAdapter(private val listItems: ArrayList<ListItem>):RecyclerView.Adapter<CurrentListAdapter.ViewHolder>() {
+/*  By Benjamin Gomori Code Review
+    I don't think that the RecyclerView Adapter should not be using firebase directly.
+    As I understand the RecyclerView Adapter - it should use ready data to populate the recycle view.
+    The ViewModels should be the centralized location for feeding live and firebase data.
+*/
+class CurrentListAdapter(private val mvm: MainViewModel, private val listItems: ArrayList<ListItem>):RecyclerView.Adapter<CurrentListAdapter.ViewHolder>() {
     override fun onCreateViewHolder(
         viewGroup: ViewGroup,
         viewType: Int
@@ -29,13 +30,11 @@ class CurrentListAdapter(private val listItems: ArrayList<ListItem>):RecyclerVie
         val currentRank: TextView
         val voteButton: ImageButton
 
-
         init {
             listItemTitle = view.findViewById(R.id.txt_Title)
             totalVotes = view.findViewById(R.id.txt_TotalVotes)
             currentRank = view.findViewById(R.id.txt_Rank)
             voteButton = view.findViewById(R.id.btn_Vote)
-
         }
     }
 
@@ -44,43 +43,23 @@ class CurrentListAdapter(private val listItems: ArrayList<ListItem>):RecyclerVie
         holder.listItemTitle.text = listItems[position].title
         holder.totalVotes.text = listItems[position].totalVotes.toString()
         holder.currentRank.text = (position + 1).toString()
+        var voteCount = listItems[position].totalVotes
 
-        holder.voteButton.setOnClickListener{
-            val firestore : FirebaseFirestore
-            firestore = FirebaseFirestore.getInstance()
-            firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
-
-            addVote(holder.listItemTitle.text.toString(), firestore)
-
-
-
+        holder.voteButton.setOnClickListener(){
+            /*  By Benjamin Gomori Code Review
+                [1] Once a user voted, that ViewHolder vote button should be disabled.
+                [2] There might not be a need to use synced data coming in from firebase to keep the vote count in-synced.
+                Basing the UI vote count on firebase may not be the most user friendly, as it will not update instantly.
+                Instead in such cases, when the update in data is always predictable, the UI count can be updated programmatically,
+                and data should be sent to update Firebase.
+             */
+            holder.voteButton.isClickable = false
+            holder.totalVotes.text = (voteCount + 1).toString()
+            mvm.addListItemVote(holder.listItemTitle.text.toString())
         }
     }
-
-    fun addVote(listItemToIncrement: String, firestore: FirebaseFirestore) {
-
-        var listItemDocument = firestore.document("lists/Top Fifteen Movies/MyListItems/" + listItemToIncrement)
-
-
-        var totalVotes: Number
-
-        listItemDocument.get().addOnSuccessListener {
-            totalVotes = it.getLong("totalVotes")!!
-            listItemDocument.update("totalVotes", (totalVotes.toLong() + 1))
-
-
-        }
-
-
-
-    }
-
-
 
     override fun getItemCount(): Int {
-
         return listItems.size
     }
-
-
 }
