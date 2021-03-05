5b2b8a79cd93c8038ebd6c6faa8f1cf7bae93aa7
benjamin gomori <benjamingomori@gmail.com>
2021-03-05 03:37:27 -0500

    No need to use @SerializedName if the names are the same

--- app/src/main/java/edu/uc/groupProject/topten/DTO/Strawpoll.kt
+++ app/src/main/java/edu/uc/groupProject/topten/DTO/Strawpoll.kt
@@ -2,6 +2,6 @@ package edu.uc.groupProject.topten.DTO
 
 import com.google.gson.annotations.SerializedName
 
-data class Strawpoll(@SerializedName("id") var id: Int, @SerializedName("title") var title: String,
+data class Strawpoll(var id: Int, var title: String,
                      var votes: ArrayList<Int>, var topTenListID: Int = 0) {
 }
