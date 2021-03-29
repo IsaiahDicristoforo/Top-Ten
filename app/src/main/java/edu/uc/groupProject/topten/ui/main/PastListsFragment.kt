a6f42ee2a3433a08b661728d89c3628c542059a6
benjamin gomori <benjamingomori@gmail.com>
2021-03-28 22:35:58 -0400

    Adjusted Fragments to all use only MainViewModel.

--- app/src/main/java/edu/uc/groupProject/topten/ui/main/PastListsFragment.kt
+++ app/src/main/java/edu/uc/groupProject/topten/ui/main/PastListsFragment.kt
@@ -14,7 +14,7 @@ import edu.uc.groupProject.topten.R
 //Handles the past_lists_fragment
 class PastListsFragment : Fragment() {
 
-    private lateinit var viewModel: PastListsViewModel
+    private lateinit var viewModel: MainViewModel
     private lateinit var adapter : ArrayAdapter<String>
     lateinit var spinnerList : Spinner //spinner variable
 
@@ -33,18 +33,25 @@ class PastListsFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(PastListsViewModel::class.java)
-        viewModel.firestoreService.fetchListNames()
-        viewModel.firestoreService.listOfLists.observe(this, Observer{
+        activity.let {
+            viewModel = ViewModelProvider(it!!).get(MainViewModel::class.java)
+        }
+        viewModel.fetchListNames()
+
+        var listOfLists = viewModel.getListOfLists()
+
+        listOfLists.observe(this, Observer{
 
             spinnerList = view!!.findViewById<Spinner>(R.id.spn_listNames)
-            adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, viewModel.firestoreService.listOfLists.value!!)
+            adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, it)
 
 
         })
         //adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, viewModel.firestoreService.listOfLists.value!!)
 
-        viewModel.firestoreService.list.observe(this, Observer {
+        var itemList = viewModel.getItemList()
+
+        itemList.observe(this, Observer {
 
             activity?.runOnUiThread(
 
