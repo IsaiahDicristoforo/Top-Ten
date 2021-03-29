e0d98b5e6fac06fb531c3f3451417c3878dfede2
DrSlugger <kylemarler@outlook.com>
2021-03-29 01:54:59 -0400

    First UI Tests

---
+++ app/src/test/java/edu/uc/groupProject/topten/service/FirestoreServiceTest.kt
@@ -0,0 +1,12 @@
+package edu.uc.groupProject.topten.service
+
+import junit.framework.TestCase
+
+class FirestoreServiceTest : TestCase() {
+
+    public override fun setUp() {
+        super.setUp()
+    }
+
+    public override fun tearDown() {}
+}
