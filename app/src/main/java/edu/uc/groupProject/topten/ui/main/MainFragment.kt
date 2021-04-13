ecf71ca539d36e2985f14801683c59ea5441e1b5
Isaiah Dicristoforo <48969369+IsaiahDicristoforo@users.noreply.github.com>
2021-04-12 23:06:56 -0400

    Fixing merge conflict errors

--- app/src/main/java/edu/uc/groupProject/topten/ui/main/MainFragment.kt
+++ app/src/main/java/edu/uc/groupProject/topten/ui/main/MainFragment.kt
@@ -6,9 +6,6 @@ import android.os.Bundle
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.content.Context
-import android.graphics.Color
-import android.os.Build
 import android.os.CountDownTimer
 import android.os.Parcelable
 import android.util.Log
@@ -148,7 +145,7 @@ class MainFragment : Fragment() {
       */
 
 
-        adapter = CurrentListAdapter(viewModel, testList)
+        adapter = CurrentListAdapter(viewModel, testList, activity!!)
         recyclerView.adapter = adapter
 
 
