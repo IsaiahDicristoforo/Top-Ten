cce7b3e09fac564b5194f7a406979d28bc4ae606
cturner0098 <turne2cn@mail.uc.edu>
2021-04-20 16:26:00 -0400

    added voting functionality to private lists

--- app/src/main/java/edu/uc/groupProject/topten/ui/main/NestedRecyclerViewAdapter.kt
+++ app/src/main/java/edu/uc/groupProject/topten/ui/main/NestedRecyclerViewAdapter.kt
@@ -7,13 +7,17 @@ import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.observe
 import androidx.recyclerview.widget.RecyclerView
 import edu.uc.groupProject.topten.R
 import edu.uc.groupProject.topten.dto.Poll
 import edu.uc.groupProject.topten.dto.PollChoice
 import edu.uc.groupProject.topten.dto.PollResponse
+import edu.uc.groupProject.topten.service.PollService
 
 class NestedRecyclerViewAdapter(private var listItems: ArrayList<PollChoice>):
+
     RecyclerView.Adapter<NestedRecyclerViewAdapter.ViewHolder>() {
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(viewGroup.context).inflate(
@@ -36,6 +40,7 @@ class NestedRecyclerViewAdapter(private var listItems: ArrayList<PollChoice>):
         val itemTitle = view.findViewById<TextView>(R.id.lbl_privateListItemTitle)
         val  totalVotesLabel  = view.findViewById<TextView>(R.id.txt_privateTotalVotes)
         val voteButton = view.findViewById<Button>(R.id.btn_privateVote)
+
     }
 
     /**
@@ -45,10 +50,22 @@ class NestedRecyclerViewAdapter(private var listItems: ArrayList<PollChoice>):
      * @param position its current position in the list
      */
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var poll = PollService()
 
         holder.itemTitle.text = listItems[position].choice
         holder.totalVotesLabel.text = listItems[position].votes.toString()  + " votes"
 
+        holder.voteButton.setOnClickListener{
+            var url = listItems[position].url
+            var urlArrary = url?.split('/')
+
+            var votedPoll = poll.castVote(urlArrary[2].toInt(), urlArrary[4].toInt())
+
+            var votes = listItems[position].votes
+            votes = votes + 1
+
+            holder.totalVotesLabel.text = votes.toString()  + " votes"
+        }
     }
 
     /**
